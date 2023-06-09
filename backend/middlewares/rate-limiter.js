a6c2fc37fdaecfc1607e6cb5da50effc62add520
Petrov Nikolai <rusgans1@yandex.ru>
2023-06-09 19:38:47 +0300

    fixLimiter

--- backend/middlewares/rate-limiter.js
+++ backend/middlewares/rate-limiter.js
@@ -1,7 +1,7 @@
 const rateLimiter = require('express-rate-limit');
 
 const limiter = rateLimiter({
-  max: 20,
+  max: 50,
   windowMS: 10000, // 10 seconds
   message: "'В данный момент вы больше не можете делать никаких запросов. Попробуйте еще раз позже",
 });
